5df81cb3f1a2c0cebb19a670861fc222c8579815
MarselDokubo <connectmarsel@gmail.com>
2025-04-11 15:01:06 +0100

    mock data

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 
 import express from "express";
 import { engine } from "express-handlebars";
+import { join } from "node:path"
+import fortunes from "./lib/data.json" assert {type: "json"};
 
+
+const __dirname = import.meta.dirname;
 const app = express();
 const PORT = 3030;
 const HOST = "[::1]"
@@ -10,9 +14,12 @@ app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 
-
+app.use(express.static(join(__dirname, "/public")))
 app.get('/', (req, res) => res.render('home'));
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]    
+    res.render('about', {fortune: randomFortune})
+})
 // custom 404 page
 app.use((req, res) => {
 res.status(404)
